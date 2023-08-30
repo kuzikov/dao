97853e428ddbcfd4b12ad2d1d2f823ab76ea6335
Adam Kuzikov <kuzikov.dev@protonmail.com>
2023-08-30 17:19:04 +0000

    add: safety checks and DAO member retrieve

--- src/dao_backend/index.ts
+++ src/dao_backend/index.ts
@@ -1,22 +1,45 @@
-import { $query, $update, Principal, ic, StableBTreeMap, Opt } from 'azle';
+import { $query, $update, Principal, ic, StableBTreeMap, Opt, Vec } from 'azle';
 import { User } from "./types";
+
 $query;
 export function whoami(): Principal {
     return ic.caller();
 }
 
+// offset, maxKeySize, maxValueSize
+// `starting` offset for stable memory is 0 sinse this is 
+// only stable memory area.
+//
+// maxKeySize is set to 64 for alingment purpose. Principal itself 
+// can be up to 63 characters.
+//
+// maxValueSize
 const DAO_MEMBERS = new StableBTreeMap<Principal, User>(0, 64, 32);
 
 $update;
-export function addMember(p: Principal): Opt<User> {
+export function addDAOMember(p: Principal, usnername: string): Opt<User> {
     if (p.isAnonymous()) {
         return Opt.None;
     };
-    const member: User = { username: p.toText() }
-    DAO_MEMBERS.insert(p, member)
 
-    return Opt.Some(member)
+    if (p !== ic.caller()) {
+        ic.trap(`unexpected Principal. want: ${ic.caller().toText()} got${p.toText()}`)
+    }
 
+
+    if (usnername.length > 32) {
+        return ic.trap(`username exceeds 32 character limit with length ${p.toString().length}`);
+    };
+
+    const member: User = { username: p.toText(), registrationDate: new Date };
+    DAO_MEMBERS.insert(p, member);
+
+    return Opt.Some(member);
+};
+
+$query
+export function getMyProfile(myPrincipal: Principal): Opt<User> {
+    return getMemberByPrincipal(myPrincipal);
 };
 
 $query
@@ -32,3 +55,10 @@ export function getMemberByPrincipal(p: Principal): Opt<User> {
     return member;
 
 };
+
+$query
+export function getDAOMembers(): Vec<User> {
+    const members = DAO_MEMBERS.values();
+    return members;
+};
+
--- src/dao_backend/types.ts
+++ src/dao_backend/types.ts
@@ -2,4 +2,5 @@
 import { Principal, Record } from "azle";
 export type User = Record<{
     username: string;
+    registrationDate: Date;
 }>;
