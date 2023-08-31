60143c288f1b76bebb59445cd64472cef1c5f480
Adam Kuzikov <kuzikov.dev@protonmail.com>
2023-08-31 18:25:39 +0000

    dik

--- src/dao_frontend/index.ts
+++ src/dao_frontend/index.ts
@@ -1,5 +1,20 @@
 import { dao_backend } from "../declarations/dao_backend";
 import { AuthClient } from '@dfinity/auth-client';
-import { login } from "./login";
 
-const log = login();
+const login = async () => {
+    const authClient = await AuthClient.create();
+    const isLocalNetwork = process.env.DFX_NETWORK == 'local';
+    const identityProviderUrl = isLocalNetwork
+        ? `http://127.0.0.1:4943/?canisterId=${process.env.CANISTER_ID_INTERNET_IDENTITY}`
+        : 'https://identity.ic0.app/';
+
+    authClient.login({
+        identityProvider: identityProviderUrl,
+        maxTimeToLive: BigInt(7 * 24 * 60 * 60 * 1000 * 1000 * 1000),
+        onSuccess: async () => {
+            const identity = await authClient.getIdentity();
+            const principal = identity.getPrincipal().toText();
+            // handle auth here
+        },
+    });
+};
